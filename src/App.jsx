e0b974ae77f1a082c5c70933bec7d190a7834d98
Rahul <its.rahulranaji@gmail.com>
2026-08-06 15:42:22 +0530

    Code 2 Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ function App(){
   return(
     <>
       <h1>Hello, React!</h1>
+      <Fruit />
+      <p>Price is {sum()}</p>
     </>
   )
 }
 
+function Fruit (){
+  return(
+    <h3>Apple is a delicious fruit!</h3>
+  )
+}
+
+function sum(){
+  return(20+34)
+}
 export default App;
