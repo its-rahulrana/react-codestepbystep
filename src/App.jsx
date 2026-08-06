c571fef4f7830f1feac91e345de98ad4fecf52a3
Rahul <its.rahulranaji@gmail.com>
2026-08-06 17:35:21 +0530

    Code 5 function on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 function App() {
+
+  function handleClick(name) {
+    alert(name == "Apple"?"Fruit":"Animal");
+  }
+
+  
   return (
     <>
-      <h1>JSX Excercise</h1>
-      <img src="src\assets\hero.png" alt="Hero" style={{ width: '100px', height: 'auto' }} />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Reherse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
-      <button onClick={() => alert('Button clicked!')}>Click me!</button>
+      <button onClick={() => handleClick("Apple")}>Apple</button>
+      <br/>
+      <button onClick={() => handleClick("Elephant")}>Elephant</button>
     </>
   );
 }
