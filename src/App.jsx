e35980157890884a20ca2d6f65b9a116a0bb6058
Rahul <its.rahulranaji@gmail.com>
2026-08-07 13:12:06 +0530

    Code 12 Handle Checkbox in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
 import { useState } from "react";
 
 function App(){
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
 
-  function clickHandler(){
-    setName("");
-    setEmail("");
-    setPass("")
-  }
+  const [skills, setSkills] = useState([]);
+
+  const changeHandler = (event) =>{
+    if (event.target.checked){
+      setSkills([...skills, event.target.value])
+    }else{
+      setSkills(skills.filter((item)=>(item != event.target.value)))
+    }
+  } 
 
   return(
     <>
-      <h1>Controlled Component</h1>
-      <p>We have to get the value before submit button</p>
-      <form action="" method="get">
-        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
-        <br/>
-        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <br/>
-        <input type="password" value={pass} onChange={(e)=>setPass(e.target.value)}/>
-        <br/>
-        <button onClick={()=>clickHandler}>Clear</button>
-        <br/>
-        
-      </form>
-      <br/>
-      <ul style={{textAlign: "left"}}>
-          <li>Name: {name}</li>
-          <li>Email: {email}</li>
-          <li>Password: {pass}</li>
-      </ul>
-    </> 
+      <div>
+        <h1>Handle Checkbox in React</h1>
+        <div style={{textAlign: "left", border: "1px solid grey", borderRadius: "10px", padding: "20px"}}>
+          <input onChange={changeHandler} type="checkbox" id="react" value="React"/>
+          <label htmlFor="react"> React </label>
+          <br/>
+          <input onChange={changeHandler} type="checkbox" id="node" value="Node"/>
+          <label htmlFor="node"> Node </label>
+          <br/>
+          <input onChange={changeHandler} type="checkbox" id="js" value="JavaScript"/>
+          <label htmlFor="js"> JavaScript </label>
+          <br/>
+          <input onChange={changeHandler} type="checkbox" id="bootstrap" value="Boot-Strap"/>
+          <label htmlFor="bootstrap"> Boot-Strap </label>
+        </div>
+        <h3>{skills}</h3>
+      </div>
+    </>
   )
 }
 
