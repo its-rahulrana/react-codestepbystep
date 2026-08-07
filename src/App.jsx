9998801e94032f7c94d82c5f147c5ba6bf3f63ac
Rahul <its.rahulranaji@gmail.com>
2026-08-07 14:08:30 +0530

    Code 14 Loop with Map Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
-import { useState } from "react";
 
 
 function App(){
-  const [gender, setGender] = useState();
-  const [city, setCity] = useState();
+
+  const user = [
+    {
+      id: "101",
+      name: "Rahul",
+      email: "rahul@react.com",
+      city: "Delhi"
+    },
+    {
+      id: "102",
+      name: "Amit",
+      email: "amit@react.com",
+      city: "Mumbai"
+    },
+    {
+      id: "103",
+      name: "Abhishek",
+      email: "abhishek@react.com",
+      city: "Gurgaon"
+    },
+    {
+      id: "104",
+      name: "Shivansh",
+      email: "shivansh@react.com",
+      city: "Patna"
+    }
+  ]
 
   return(
     <>
-      <h1>Handle Radio Button & Dropdown</h1>
-      <div style={{textAlign:"left", border: "1px solid green", margin: "10px", padding:" 20px", borderRadius: "10px"}}>
-        <h3>Radio Button</h3>
-        <h4>Gender: {gender}</h4>
-        <input onChange={(e)=>setGender(e.target.value)} type="radio" name="gender" value="Male"/>
-        <label htmlFor="male">Male</label>
-        <input onChange={(e)=>setGender(e.target.value)} type="radio" name="gender" value="Female"/>
-        <label htmlFor="female">Female</label>
-      </div>
-      <div style={{textAlign:"left", border: "1px solid green", margin: "10px", padding:" 20px", borderRadius: "10px"}}>
-        <h3>Dropdown List</h3>
-        <h4>City: {city}</h4>
-        <select onChange={(e)=>(setCity(e.target.value))} defaultValue="Noida">
-          <option value="Delhi">Delhi</option>
-          <option value="Gurgaon">Gurgaon</option>
-          <option value="Faridabad">Faridabad</option>
-          <option value="Noida">Noida</option>
-        </select>
-      </div>
+    <h1>Loop with Map Function</h1>
+    <table style={{border: "1px solid grey", margin: "10px"}}>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          user.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.city}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
     </>
   )
 }
