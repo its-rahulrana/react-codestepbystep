07412c7f3ab67db7f684a901fcea92af02b71bac
Rahul <its.rahulranaji@gmail.com>
2026-08-07 17:04:21 +0530

    Code 16 Props Task: Clock with Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,29 @@
-import User from "./User";
-
+import {useState, useEffect} from 'react';
 
 function App(){
 
-  const user = [
-    {
-      id: "101",
-      name: "Rahul",
-      email: "rahul@react.com",
-      city: "Delhi"
-    },
-    {
-      id: "102",
-      name: "Amit",
-      email: "amit@react.com",
-      city: "Mumbai"
-    },
-    {
-      id: "103",
-      name: "Abhishek",
-      email: "abhishek@react.com",
-      city: "Gurgaon"
-    },
-    {
-      id: "104",
-      name: "Shivansh",
-      email: "shivansh@react.com",
-      city: "Patna"
-    }
-  ]
+  const [clock, setClock] = useState(new Date().toLocaleTimeString());
+  const [clockColor, setClockColor] = useState("blue");
+
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setClock(new Date().toLocaleTimeString())
+    }, 1000)
+  })
 
   return(
     <>
-    <h1>Reuse Component in Loop</h1>
-      {
-        user.map((item, index)=>(
-          <User user={item} key={index}/>
-        ))
-      }
+      <h1>Props Task</h1>
+      <p style={{margin: "50px", background: clockColor, width:"200px", padding: "10px", borderRadius: "10px", fontSize: "30px", color:"black"}}>{clock}</p>
+
+      <select onChange={(e)=>setClockColor(e.target.value)} style={{width: "200px", padding:"5px"}}>
+        <option value="Blue">Blue</option>
+        <option value="Orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="red">Red</option>
+      </select>
     </>
   )
 }
