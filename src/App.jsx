ac3374b716b4c5ec2ef02f849d614e9bb47abe49
Rahul <its.rahulranaji@gmail.com>
2026-08-07 09:25:09 +0530

    Code 11 Controlled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from "react";
 
 function App(){
-  const [val, setVal] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  function clickHandler(){
+    setName("");
+    setEmail("");
+    setPass("")
+  }
 
   return(
     <>
-      <h1>Input field and on change event</h1>
-      <input value={val} onChange={(e)=>setVal(e.target.value)} type="text" style={{width: "200px", margin:"20px", padding: "10px"}}/>
-      <button onClick={()=>setVal("")} style={{width: "200px", margin:"20px", padding: "10px"}}>Clear</button>
-      <h3>{val}</h3>
+      <h1>Controlled Component</h1>
+      <p>We have to get the value before submit button</p>
+      <form action="" method="get">
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <br/>
+        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <br/>
+        <input type="password" value={pass} onChange={(e)=>setPass(e.target.value)}/>
+        <br/>
+        <button onClick={()=>clickHandler}>Clear</button>
+        <br/>
+        
+      </form>
+      <br/>
+      <ul style={{textAlign: "left"}}>
+          <li>Name: {name}</li>
+          <li>Email: {email}</li>
+          <li>Password: {pass}</li>
+      </ul>
     </> 
   )
 }
