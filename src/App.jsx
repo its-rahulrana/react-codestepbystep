a3c351c53bff430d513d724c5ddda564df19760f
Rahul <its.rahulranaji@gmail.com>
2026-08-09 09:35:09 +0530

    Code 21 Dynamic and conditional Inline style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 
 
 function App(){
 
-  const cardStyle = {
+  const [cardStyle, setCardStyle] = useState({
     border: "1px solid grey",
     width: "300px",
     boxShadow: "1px white",
     margin: "10px",
     padding: "10px",
-    borderRadius: "10px"
+    borderRadius: "10px",
+    backgroundColor: ""
+  });
+
+  const [blockDisplay, setBlockDisplay] = useState("flex");
+
+  const themeHandler = () => {
+    cardStyle.backgroundColor === ""?
+    setCardStyle({...cardStyle, backgroundColor:"#ffabab", color:"rgb(0, 46, 92)"}): cardStyle.backgroundColor === "#ffabab"?
+    setCardStyle({...cardStyle, backgroundColor: "rgb(149, 202, 255)", color:"rgb(187, 0, 0)"}):
+    setCardStyle({...cardStyle, backgroundColor:"", color:""})
   }
 
   return(
     <>
       <h1>Inline Style</h1>
-      <div style={{display:"flex", flexWrap: "wrap"}}>
+      <button onClick={themeHandler}>Theme</button>
+        <button onClick={()=>blockDisplay === "flex" ?setBlockDisplay("block"):setBlockDisplay("flex")}>Boxing</button>
+      <div style={{display:blockDisplay, flexWrap: "wrap"}}>
         <div style={cardStyle}>
           <img src="src\assets\hero.png" style={{width:"250px"}}/>
           <div>
