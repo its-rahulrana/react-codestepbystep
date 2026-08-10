f7235b2dd83825322fc02c13d1efc8f91e652f06
Rahul <its.rahulranaji@gmail.com>
2026-08-10 22:54:43 +0530

    Code 26  useRef Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { Button } from "react-bootstrap";
+import { useRef } from "react";
 
 function App(){
   
+  const inputRef = useRef(null);
+  const paraRef = useRef();
+
+  const inputHandler = () =>{
+    inputRef.current.focus();
+    inputRef.current.style.color = 'red';
+    inputRef.current.value = "12345"
+  }
+
+  const handleToggle = () => {
+    inputRef.current.style.display === 'none'?
+    inputRef.current.style.display = 'inline':
+    inputRef.current.style.display = 'none'
+  }
+
+  const paraHandler = () => {
+    paraRef.current.style.color = 'skyblue';
+    paraRef.current.style.padding = '20px';
+    paraRef.current.style.backgroundColor = 'yellow'
+  }
+
   return(
     <>
-      <h1>Bootstrap with React</h1>
-      <button>Normal Button</button>
+      <h1>useRef Hook</h1>
+      <div style={{width: "100%"}}>
+        <button onClick={handleToggle}>Toggle</button>
+        <input ref={inputRef} type="text" />
+        <button onClick={inputHandler}>Focus on Input</button>
+      </div>
       <br/>
-      <Button>Bootstrap Button</Button>
       <br/>
-      <Button variant="success">Bootstrap button with variant: success</Button>
+      <button onClick={paraHandler} style={{width:"200px"}}>Paragraph Button</button>
+      <p ref={paraRef}>This a sentence to check Paragraph refrence.</p>
     </>
   )
 }
