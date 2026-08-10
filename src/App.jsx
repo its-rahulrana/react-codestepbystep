b90f56c6006ff1898f9e3a223fcf5985789427af
Rahul <its.rahulranaji@gmail.com>
2026-08-10 13:08:56 +0530

    Code 24 Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import User from "./User";
-
+import styled from "styled-components";
 
 function App(){
+
+  const Heading3 = styled.h3`
+  color: #aa0404;
+  background-color: #8acbff;
+  padding: 5px
+  `
+
   return(
     <>
-      <h1>Style with CSS Module</h1>
-      <p>Paragraph with same class name as in User Component</p>
-      <User/>
+      <h1>Styled Component</h1>
+      <h3>Normal heading 3</h3>
+      <Heading3>This is heading 3 from styled component</Heading3>
     </>
   )
 }
