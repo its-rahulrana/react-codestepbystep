3280f0625c567083acaf30383c48d8c51d034ff9
Rahul <its.rahulranaji@gmail.com>
2026-08-11 13:11:17 +0530

    Code 29 ForwardRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import User from "./User";
+import { useRef } from "react";
+import UserInput from "./UserInput";
 
 const App = () => {
 
-  const userHandler = name => {
-    alert(`Your name is : ${name}`)
-  };
+  const inputRef = useRef();
 
+  const inputRefHandler = () => {
+    inputRef.current.focus(),
+    inputRef.current.value = "12345",
+    inputRef.current.style.color = "blue",
+    inputRef.current.style.width = "400px"
+  }
+  
   return(
     <>
-      <h1>Pass Function in Component as Props</h1>
-      {/* <User userHandler = {userHandler}/> */}
-      <User userHandler = {userHandler} name="Amit"/>
+      <h1>frowardRef</h1>
+      <UserInput ref={inputRef}/>
+      <button onClick={inputRefHandler}>Click Here</button>
     </>
   )
 }
