2e1220b1948880054816e90b3b46de39697b83f8
Rahul <its.rahulranaji@gmail.com>
2026-08-12 12:58:08 +0530

    Code 32 Derived State

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { useTransition } from "react";
+import { useState } from "react";
 
 const App = () => {
 
-  const [pending, startTransition] = useTransition();
+  const [users, setUsers] = useState([]);
+  const [userName, setUserName] = useState();
 
-  const buttonHandler = () => {
-    startTransition(async()=>{
-      await new Promise(res => setTimeout(res, 3000))
-    })
+  const addHandler = () => {
+    setUsers([...users, userName]);
   }
-  
+
+  const totalUser = users.length;
+  const lastUser = users[users.length-1];
+  const uniqueUser = [...new Set(users)].length;
+
   return(
     <>
-      <h1>useTransition Hook</h1>
+      <h1>Derived State in React</h1>
+      <h3>Total User : {totalUser} </h3>
+      <h3>Last User : {lastUser}</h3>
+      <h3>Unique User : {uniqueUser}</h3>
+      <hr/>
+      <div>
+        <input value={userName} onChange={(e)=>setUserName(e.target.value)} type="text"/>
+        <button onClick={addHandler}>Add User</button>
+      </div>
+      <hr/>
       {
-        pending && <img style={{width: "100px"}} src="src\assets\Loading_icon.gif"/>
+        users.map((item, index) => (
+          <h5 key={index}>{item}</h5>
+        ))
       }
-      <button disabled={pending} onClick={buttonHandler}>Click</button>
     </>
   )
 }
