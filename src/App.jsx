3c2c0eea31a4bf3101f816d6afe146caf506b5b2
Rahul <its.rahulranaji@gmail.com>
2026-08-18 13:35:10 +0530

    Code 37 userId Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,32 @@
-import { useActionState } from "react";
+import { useId } from "react";
 
 const App = () => {
-
-  const submitHandler = async(previousData, formData) => {
-    let name = formData.get('name');
-    let password = formData.get('pass')
-    
-    await new Promise (res => setTimeout(res,2000))
-    console.log(name, password)
-
-    if(name && password){
-      return {message: "Data Submitted", name, password}
-    } else {
-      return {error: "Failed to submit, Enter proper data"}
-    }
-  }
-
-  const [data, action, pending] = useActionState(submitHandler, undefined);
-  
-  return(
+  return (
     <>
-      <h1>useActionState Hook</h1>
-      <form action={action}>
-        <input type="text" placeholder="User Name" name="name"/>
-        <br/>
-        <br/>
-        <input type="password" placeholder="Enter Password" name="pass"/>
-        <br/>
-        <br/>
-        <button disabled={pending}>Submit</button>
-        <br/>
-        {
-          data?.error && <span style={{color: "red"}}>{data.error}</span>
-        }
-        {
-          data?.message && <span style={{color: "green"}}>{data.message}</span>
-        }
-      </form>
-      <h3>Name: {data?.name}</h3>
-      <h3>Password: {data?.password}</h3>
+      <h1>useId Hook</h1>
+      <UserForm/>
+      <hr/>
+      <UserForm/>
     </>
   )
-};
+}
 
 export default App;
+
+const UserForm = () => {
+  const userId = useId();
+  return(
+    <form>
+      <input type="text" id={userId+"name"} placeholder="Name"/>
+      <br/>
+      <br/>
+      <input type="email" id={userId+"email"} placeholder="Email"/>
+      <br/>
+      <br/>
+      <input type="password" id={userId+"pass"} placeholder="Password"/>
+      <br/>
+      <br/>
+      <button>Submit</button>
+    </form>
+  )
+}
